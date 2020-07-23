b8d6eefc1afa73461195d6266ba7b6f5ef896ca9
Josiah Bull <josiah.bull7@gmail.com>
2020-07-23 12:28:10 +1200

    ESP32 Control Code
    Arduino code removed as no longer needed.

--- ESP32 Firmware/BasicControl/BasicControl.ino
+++ ESP32 Firmware/BasicControl/BasicControl.ino
@@ -1,145 +1,216 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
+#include "WiFi.h"
+#include "ESPAsyncWebServer.h"
+#include <EEPROM.h>
+#include "AsyncJson.h"
 
-// Load Wi-Fi library
-#include <WiFi.h>
-
-// Replace with your network credentials
-const char* ssid     = "BullFamilyWifi";
+// Quick Config
+const char* ssid = "BullFamilyWifi";
 const char* password = "blessedtobless";
+const int closedSwitchPin = 33;
+const int openSwitchPin = 18;
+const int relayOnePin = 4;
+const int relayTwoPin = 2;
+//const int statusLedPin = 13;
 
-// Set web server port number to 80
-WiFiServer server(80);
+//Global Variables
+unsigned long motorTimeout = 8500;
+bool error;
 
-// Variable to store the HTTP request
-String header;
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
-// Auxiliar variables to store the current output state
-String output26State = "off";
-String output27State = "off";
-
-// Assign output variables to GPIO pins
-const int output26 = 26;
-const int output27 = 27;
-
-void setup() {
-  Serial.begin(115200);
-  // Initialize the output variables as outputs
-  pinMode(output26, OUTPUT);
-  pinMode(output27, OUTPUT);
-  // Set outputs to LOW
-  digitalWrite(output26, LOW);
-  digitalWrite(output27, LOW);
-
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
+//Functions
+void errorOccured() {
+  //To be called when an error is detected. Will permenantly disable device until reset signal recieved.
+  error = true;
+  EEPROM.write(2, 1);
+}
+void resetError() {
+  //Resets Error
+  error = false;
+  EEPROM.write(2, 0);
 }
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0) {
-              Serial.println("GPIO 26 on");
-              output26State = "on";
-              digitalWrite(output26, HIGH);
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-              output26State = "off";
-              digitalWrite(output26, LOW);
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-              output27State = "on";
-              digitalWrite(output27, HIGH);
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-              output27State = "off";
-              digitalWrite(output27, LOW);
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 26  
-            client.println("<p>GPIO 26 - State " + output26State + "</p>");
-            // If the output26State is off, it displays the ON button       
-            if (output26State=="off") {
-              client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/26/off\"><button class=\"button button2\">OFF</button></a></p>");
-            } 
-               
-            // Display current state, and ON/OFF buttons for GPIO 27  
-            client.println("<p>GPIO 27 - State " + output27State + "</p>");
-            // If the output27State is off, it displays the ON button       
-            if (output27State=="off") {
-              client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
-            } else {
-              client.println("<p><a href=\"/27/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
+//Declare Classes
+class Button {
+  bool _pressed;
+  int _pin;
+  public:
+    Button(int pin) : _pin(pin) {
+      pinMode(_pin, OUTPUT);
+      digitalWrite(_pin, LOW);
+    };
+    bool pressed() {
+      if (digitalRead(_pin) == HIGH) {
+        _pressed = true;
+      } else {
+        _pressed = false;
       }
+      return _pressed;
     }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
-  }
+};
+
+class Motor {
+  int _relayPinOne;
+  int _relayPinTwo;
+  Button _closeSwitch;
+  Button _openSwitch;
+  bool _running;
+  bool _opening;
+  bool _open;
+  unsigned long _startTime;
+  
+  public:
+    Motor(int relayOne, int relayTwo, Button closeSwitch, Button openSwitch) : _relayPinOne(relayOne), _relayPinTwo(relayTwo), _closeSwitch(closeSwitch), _openSwitch(openSwitch), _running(false), _opening(false), _startTime(0) {
+      pinMode(_relayPinOne, OUTPUT);
+      pinMode(_relayPinTwo, OUTPUT);
+      digitalWrite(_relayPinOne, HIGH);
+      digitalWrite(_relayPinTwo, HIGH);
+      //Check if vane is open or closed on initalisation, if not sure then run open command until fully open.
+      if (_closeSwitch.pressed() == true) {
+        _open = false;
+      }
+      if (_openSwitch.pressed() == true) {
+        _open = true;
+      }
+      if (_openSwitch.pressed() == false && _closeSwitch.pressed() == false) {
+        _open = false;
+        openShutter();
+      }
+      
+    };
+    void closeShutter() {
+      if (!_open) return;
+      if (error) return;
+      _startTime = millis();
+      _running = true;
+      _opening = false;
+      digitalWrite(_relayPinOne, HIGH);
+      digitalWrite(_relayPinTwo, LOW);
+    };
+    
+    void openShutter() {
+      if (_open) return;
+      if (error) return;
+      _startTime = millis();
+      _running = true;
+      _opening = true;
+      digitalWrite(_relayPinOne, LOW);
+      digitalWrite(_relayPinTwo, HIGH);
+    };
+
+    void msCheck(unsigned long currentTime) {
+      if (_running) { //Check motor is running
+        if (_opening) { //Check if motor if opening or closing, and act accordingly.
+          if (_openSwitch.pressed() == true || currentTime - _startTime >= motorTimeout) {
+            _open = true;
+            _running = false;
+            digitalWrite(_relayPinOne, HIGH);
+            digitalWrite(_relayPinTwo, HIGH);
+          };
+          if (currentTime - _startTime >= motorTimeout) {
+            errorOccured();
+          };
+        } else {
+          if (_closeSwitch.pressed() == true || currentTime - _startTime >= motorTimeout) {
+            _open = false;
+            _running = false;
+            digitalWrite(_relayPinOne, HIGH);
+            digitalWrite(_relayPinTwo, HIGH);
+          };
+          if (currentTime - _startTime >= motorTimeout) {
+            errorOccured();
+          };
+        };
+      };
+    };
+    bool getStatus() { 
+      return _open;
+    }
+};
+
+class Led {
+  int _pin;
+  bool _on;
+  
+  public: 
+    Led(int pin) : _pin(pin), _on(false) {
+      pinMode(_pin, OUTPUT);
+      digitalWrite(_pin, LOW);
+    };
+    void off() {
+      _on = false;
+      digitalWrite(_pin, LOW);
+    };
+    void on() {
+      _on = true;
+      digitalWrite(_pin, HIGH);
+    };
+    void toggle() {
+      if (_on) {
+        off();
+      } else {
+        on();
+      }
+    };
+    bool isOn() {
+      return _on;  
+    };
+};
+
+//Instantiate Things
+Button switchOne(closedSwitchPin);
+Button switchTwo(openSwitchPin);
+Motor mainMotor(relayOnePin, relayTwoPin, switchOne, switchTwo);
+//Led statusLed(statusLedPin);
+ 
+void setup(){  
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  //Load Error Status on startup.
+  if (EEPROM.read(2) == 1) {
+    errorOccured();
+  };
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  } 
+  
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+  // Gets the status page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    Serial.println("Status Request Received");
+    request->send(200, "application/json", "{\"shutterOpen\":\"" + String(mainMotor.getStatus()) + "\", \"errorStatus\":\"" + String(error) + "\"}");
+  });
+
+  // Route to open shutter
+  server.on("/open", HTTP_POST, [](AsyncWebServerRequest *request){
+    mainMotor.openShutter();
+    request->send(200, "application/json", "{\"shutterOpen\":\"" + String(mainMotor.getStatus()) + "\", \"errorStatus\":\"" + String(error) + "\"}");
+  });
+  
+  // Route to close shutter
+  server.on("/close", HTTP_POST, [](AsyncWebServerRequest *request){
+    mainMotor.closeShutter(); 
+    request->send(200, "application/json", "{\"shutterOpen\":\"" + String(mainMotor.getStatus()) + "\", \"errorStatus\":\"" + String(error) + "\"}");
+  });
+
+  //Route to reset the shutter from an Error
+   server.on("/reset", HTTP_POST, [](AsyncWebServerRequest *request){
+    resetError();
+    request->send(200, "application/json", "{\"shutterOpen\":\"" + String(mainMotor.getStatus()) + "\", \"errorStatus\":\"" + String(error) + "\"}");
+  });
+  
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+  mainMotor.msCheck(millis());
+  delay(250);
 }
